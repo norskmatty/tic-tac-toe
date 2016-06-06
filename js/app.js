e3a0fbcf742fc62b27d06490c7264bc70ad2378c
norskmatty <matthewconley00@gmail.com>
2016-06-06 16:41:08 -0700

    commit

---
+++ js/app.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+	var turn = 0;
+	var xwin = 0;
+	var owin = 0;
+	var ties = 0;
+
+	$('.box').on('click', function() {
+		if($(this).hasClass('X') || $(this).hasClass('O')) {
+			alert("Choose an empty square");
+		}
+		else {
+			if(turn == 0) {
+				$(this).toggleClass('O');
+				turn = 1;
+			}
+			else {
+				$(this).toggleClass('X');
+				turn = 0;
+			}
+		}
+
+		if($('.row1 .column1').hasClass('O') && $('.row2 .column1').hasClass('O') && $('.row3 .column1').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column2').hasClass('O') && $('.row2 .column2').hasClass('O') && $('.row3 .column2').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column3').hasClass('O') && $('.row2 .column3').hasClass('O') && $('.row3 .column3').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column1').hasClass('O') && $('.row1 .column2').hasClass('O') && $('.row1 .column3').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row2 .column1').hasClass('O') && $('.row2 .column2').hasClass('O') && $('.row2 .column3').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row3 .column1').hasClass('O') && $('.row3 .column2').hasClass('O') && $('.row3 .column3').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column1').hasClass('O') && $('.row2 .column2').hasClass('O') && $('.row3 .column3').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column3').hasClass('O') && $('.row2 .column2').hasClass('O') && $('.row3 .column1').hasClass('O')) {
+			alert(" 'O' wins!");
+			owin++;
+		}
+		else if($('.row1 .column1').hasClass('X') && $('.row2 .column1').hasClass('X') && $('.row3 .column1').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column2').hasClass('X') && $('.row2 .column2').hasClass('X') && $('.row3 .column2').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column3').hasClass('X') && $('.row2 .column3').hasClass('X') && $('.row3 .column3').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column1').hasClass('X') && $('.row1 .column2').hasClass('X') && $('.row1 .column3').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row2 .column1').hasClass('X') && $('.row2 .column2').hasClass('X') && $('.row2 .column3').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row3 .column1').hasClass('X') && $('.row3 .column2').hasClass('X') && $('.row3 .column3').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column1').hasClass('X') && $('.row2 .column2').hasClass('X') && $('.row3 .column3').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column3').hasClass('X') && $('.row2 .column2').hasClass('X') && $('.row3 .column1').hasClass('X')) {
+			alert(" 'X' wins!");
+			xwin++;
+		}
+		else if($('.row1 .column1').is('.X, .O') && $('.row1 .column2').is('.X, .O') && $('.row1 .column3').is('.X, .O') && $('.row2 .column1').is('.X, .O') && $('.row2 .column2').is('.X, .O') && $('.row2 .column3').is('.X, .O') && $('.row3 .column1').is('.X, .O') && $('.row3 .column2').is('.X, .O') && $('.row3 .column3').is('.X, .O'))  {
+			alert(" It's a tie!  Surprise.");
+			ties++;
+		}
+	})
+
+	$('button').on('click', function () {
+		$('.box').removeClass("X");
+		$('.box').removeClass("O");
+	})
+
+
+})
+
